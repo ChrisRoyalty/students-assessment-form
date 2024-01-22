d9986977983c012c01f550aec15e07a15cc96832
ChrisRoyalty <chrisroyalty001@gmail.com>
2024-01-22 12:09:36 +0100

    students-accessment-form

---
+++ app.js
@@ -0,0 +1,9 @@
+let studentsDetails = [
+  {
+    firstName: "Chibuikem",
+    secondName: "Christian",
+    email: "chrisroyalty001@gmail.com",
+    regNo: "123456",
+  },
+];
+console.log(studentsDetails);
